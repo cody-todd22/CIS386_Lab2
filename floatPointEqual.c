e1b6e94a3acc1e4729a11b0abc4871887fa1df23
cody-todd22 <70772864+cody-todd22@users.noreply.github.com>
2022-09-16 06:49:43 -0500

    I think this works

---
+++ floatPointEqual.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    float n;
+
+    n = 6.7;
+
+    if(n == 6.7)
+    {
+        printf("true\n");
+    } 
+    else 
+    {
+        printf("false\n");
+    }
+}
--- powerOfTwo.c
+++ powerOfTwo.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 
-int main(){}
-
 //Thank You Dr Casey and exploringbinary.com
 int isPowerOfTwoClever (unsigned int x)
 {
   return ((x != 0) && !(x & (x - 1)));
+}
+
+int main()
+{
+  int num;
+  printf("Please enter a number to check if it is a power of 2: ");
+  scanf("%d", &num);
+
+  isPowerOfTwoClever(num);
 }
